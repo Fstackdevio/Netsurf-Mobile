83ac7b0a9ef49e264c5dccc0713fb82fc7917c12
michael kalango <michaelkalango@gmail.com>
2018-12-31 15:40:00 +0100

    middlewear api consumer clean up || errors in identifiying installed plugins

--- lib/api/consumer.dart
+++ lib/api/consumer.dart
@@ -6,48 +6,45 @@ import 'package:netsurf_pro/models/Quota.dart';
 import 'package:netsurf_pro/models/base/EventObject.dart';
 import 'package:netsurf_pro/models/ApiRequest.dart';
 import 'package:netsurf_pro/models/ApiResponse.dart';
+import 'package:netsurf_pro/tools/constants.dart';
 
 Future<EventObject> loginUser(String details, String password) async {
   ApiRequest apiRequest = new ApiRequest();
-  User user new User(username: details, email: details, password: password);
+  User user = new User(username: details, email: details, password: password);
 
-  apiRequest.operation = APIOperation.LOGIN;
+  apiRequest.operation = APIOperations.LOGIN;
   apiRequest.user = user;
 
   try {
     final encoding = APIConstants.OCTET_STREAM_ENCODING;
     final response = await http.post(APIConstants.API_BASE_URL, body: json.encode(apiRequest.toJson()), encoding: Encoding.getByName(encoding));
     if(response != null) {
-      if(response.statusCode == APIConstants.SC_OK && response.body != null) {
+      if(response.statusCode == APIResponseCode.SC_OK && response.body != null) {
         final responseJson = json.decode(response.body);
         ApiResponse apiResponse = ApiResponse.fromJson(responseJson);
         if(apiResponse.result == APIOperations.SUCCESS) {
           return new EventObject(
             id: EventConstants.LOGIN_SUCCESSFUL,
-            message: apiResponse.message,
             object: apiResponse.user
           );
         }
         else {
           return new EventObject(
             id: EventConstants.LOGIN_ERR,
-            message: apiResponse.message,
           );
         }
       }
-      else if(response.statusCode == APIConstants.SC_ERR && response.body != null) {
+      else if(response.statusCode == APIResponseCode.SC_ERR && response.body != null) {
         final responseJson = json.decode(response.body);
         ApiResponse apiResponse = ApiResponse.fromJson(responseJson);
         if(apiResponse.result == APIOperations.ERR) {
           return new EventObject(
             id: EventConstants.LOGIN_ERR,
-            message: apiResponse.message,
           );
         }
         else {
           return new EventObject(
             id: EventConstants.LOGIN_ERR,
-            message: apiResponse.message,
           );
         }
       }
@@ -67,51 +64,47 @@ Future<EventObject> loginUser(String details, String password) async {
 
 Future<EventObject> buyData(String ewallet, String username, String amount, String pin) async {
   ApiRequest apiRequest = new ApiRequest();
-  Quota quotaBuy new Quota(ewallet: ewallet, username: username, amount: amount, ewallet_pin: pin);
+  Quota quota = new Quota(ewallet: ewallet, username: username, amount: amount, ewallet_pin: pin);
 
-  apiRequest.operation = APIOperation.BUYDATA;
-  apiRequest.quota = quotaBuy;
+  apiRequest.operation = APIOperations.BUY;
+  apiRequest.quota = quota;
 
   try {
     final encoding = APIConstants.OCTET_STREAM_ENCODING;
     final response = await http.post(APIConstants.API_BASE_URL, body: json.encode(apiRequest.toJson()), encoding: Encoding.getByName(encoding));
     if(response != null) {
-      if(response.statusCode == APIConstants.SC_OK && response.body != null) {
+      if(response.statusCode == APIResponseCode.SC_OK && response.body != null) {
         final responseJson = json.decode(response.body);
         ApiResponse apiResponse = ApiResponse.fromJson(responseJson);
         if(apiResponse.result == APIOperations.SUCCESS) {
           return new EventObject(
-            id: EventConstants.LOGIN_SUCCESSFUL,
-            message: apiResponse.message,
+            id: EventConstants.BUY_SUCCESSFUL,
             object: apiResponse.user
           );
         }
         else {
           return new EventObject(
-            id: EventConstants.LOGIN_ERR,
-            message: apiResponse.message,
+            id: EventConstants.BUY_ERR,
           );
         }
       }
-      else if(response.statusCode == APIConstants.SC_ERR && response.body != null) {
+      else if(response.statusCode == APIResponseCode.SC_ERR && response.body != null) {
         final responseJson = json.decode(response.body);
         ApiResponse apiResponse = ApiResponse.fromJson(responseJson);
         if(apiResponse.result == APIOperations.ERR) {
           return new EventObject(
-            id: EventConstants.LOGIN_ERR,
-            message: apiResponse.message,
+            id: EventConstants.BUY_ERR,
           );
         }
         else {
           return new EventObject(
-            id: EventConstants.LOGIN_ERR,
-            message: apiResponse.message,
+            id: EventConstants.BUY_ERR,
           );
         }
       }
       else {
         return new EventObject(
-          id: EventConstants.LOGIN_ERR
+          id: EventConstants.BUY_ERR
         );
       }
     }
@@ -125,51 +118,106 @@ Future<EventObject> buyData(String ewallet, String username, String amount, Stri
 
 Future<EventObject> shareData(String receipent, String amount, String pin) async {
   ApiRequest apiRequest = new ApiRequest();
-  Quota quotaShare new Quota(receipent: receipent, amount: amount, ewallet_pin: pin);
+  Quota quota = new Quota(receipent: receipent, amount: amount, ewallet_pin: pin);
 
-  apiRequest.operation = APIOperation.SHAREDATA;
-  apiRequest.quota = quotaShare;
+  apiRequest.operation = APIOperations.SHARE;
+  apiRequest.quota = quota;
 
   try {
     final encoding = APIConstants.OCTET_STREAM_ENCODING;
     final response = await http.post(APIConstants.API_BASE_URL, body: json.encode(apiRequest.toJson()), encoding: Encoding.getByName(encoding));
     if(response != null) {
-      if(response.statusCode == APIConstants.SC_OK && response.body != null) {
+      if(response.statusCode == APIResponseCode.SC_OK && response.body != null) {
         final responseJson = json.decode(response.body);
         ApiResponse apiResponse = ApiResponse.fromJson(responseJson);
         if(apiResponse.result == APIOperations.SUCCESS) {
           return new EventObject(
-            id: EventConstants.LOGIN_SUCCESSFUL,
-            message: apiResponse.message,
+            id: EventConstants.SHARE_SUCCESSFUL,
             object: apiResponse.user
           );
         }
         else {
           return new EventObject(
-            id: EventConstants.LOGIN_ERR,
-            message: apiResponse.message,
+            id: EventConstants.SHARE_ERR,
           );
         }
       }
-      else if(response.statusCode == APIConstants.SC_ERR && response.body != null) {
+      else if(response.statusCode == APIResponseCode.SC_ERR && response.body != null) {
         final responseJson = json.decode(response.body);
         ApiResponse apiResponse = ApiResponse.fromJson(responseJson);
         if(apiResponse.result == APIOperations.ERR) {
           return new EventObject(
-            id: EventConstants.LOGIN_ERR,
-            message: apiResponse.message,
+            id: EventConstants.SHARE_ERR,
           );
         }
         else {
           return new EventObject(
-            id: EventConstants.LOGIN_ERR,
-            message: apiResponse.message,
+            id: EventConstants.SHARE_ERR,
           );
         }
       }
       else {
         return new EventObject(
-          id: EventConstants.LOGIN_ERR
+          id: EventConstants.SHARE_ERR
+        );
+      }
+    }
+    else {
+      return new EventObject();
+    }
+  } catch (Exception) {
+    return new EventObject();
+  }
+}
+
+Future<EventObject> changePassword(String username, String old_password, String new_password, String pin) async {
+  ApiRequest apiRequest = new ApiRequest();
+  User user = new User(username: username, old_password: old_password, new_password: new_password, secret_pin: pin);
+
+  apiRequest.operation = APIOperations.CHANGE;
+  apiRequest.user = user;
+
+  try {
+    final encoding = APIConstants.OCTET_STREAM_ENCODING;
+    final response = await http.post(APIConstants.API_BASE_URL, body: json.encode(apiRequest.toJson()), encoding: Encoding.getByName(encoding));
+    if(response != null) {
+      if(response.statusCode == APIResponseCode.SC_OK && response.body != null) {
+        final responseJson = json.decode(response.body);
+        ApiResponse apiResponse = ApiResponse.fromJson(responseJson);
+        if(apiResponse.result == APIOperations.SUCCESS) {
+          return new EventObject(
+            id: EventConstants.CHANGE_SUCCESSFUL,
+            object: null
+          );
+        }
+        else if(apiResponse.result == APIOperations.ERR) {
+          return new EventObject(
+            id: EventConstants.INVALID_OLD_PASS,
+          );
+        }
+        else {
+          return new EventObject(
+            id: EventConstants.CHANGE_ERR,
+          );
+        }
+      }
+      else if(response.statusCode == APIResponseCode.SC_ERR && response.body != null) {
+        final responseJson = json.decode(response.body);
+        ApiResponse apiResponse = ApiResponse.fromJson(responseJson);
+        if(apiResponse.result == APIOperations.ERR) {
+          return new EventObject(
+            id: EventConstants.CHANGE_ERR,
+          );
+        }
+        else {
+          return new EventObject(
+            id: EventConstants.CHANGE_ERR,
+          );
+        }
+      }
+      else {
+        return new EventObject(
+          id: EventConstants.CHANGE_ERR
         );
       }
     }
